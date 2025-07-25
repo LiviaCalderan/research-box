5c029947dde26f15cbea97f06715ebeb7bce3049
Livia Calderan <liv_calderan@hotmail.com>
2025-07-24 21:10:43 -0400

    test-newSearch-http201

---
+++ spec/requests/searches_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Searches", type: :request do
+  describe "POST /searches" do
+    context "valid user_search" do
+      it "creates a new search and returns HTTP 201" do
+        headers = { "CONTENT_TYPE" => "application/json"}
+        post "/searches", params: {user_search: "testing search"}.to_json, headers: headers
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)["user_search"]).to eq("testing search")
+        expect(Search.count).to eq(1)
+      end
+    end
+  end
+end
